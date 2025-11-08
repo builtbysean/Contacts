77e21a46ab04c858f0c80b87edb63a870b4053d0
Sean Coffin <seancoffin2003@gmail.com>
2025-11-08 14:41:36 -0500

    Initial Commit

---
+++ Contacts/ContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsApp.swift
+//  Contacts
+//
+//  Created by Sean Coffin on 11/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
